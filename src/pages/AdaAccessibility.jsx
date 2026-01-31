180e7b9eaa535813ad938ab5ba504992c4161a94
Rick A Hesse <newtechad1@gmail.com>
2026-01-31 14:36:29 +0000

    File changes

--- src/pages/AdaAccessibility.jsx
+++ src/pages/AdaAccessibility.jsx
@@ -231,7 +231,7 @@ export default function AdaAccessibility() {
                 {[
                   'We fix the biggest risks',
                   'We handle the important pages',
-                  'You get a follow-up review when we're done'
+                  'You get a follow-up review when we are done'
                 ].map((item, index) => (
                   <li key={index} className="flex items-start gap-2">
                     <CheckCircle className="w-5 h-5 text-green-600 flex-shrink-0 mt-0.5" />
